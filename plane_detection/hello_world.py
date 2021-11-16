e02013e5af6482c1f6cb3b4c83f6b630bbbce12a
mbanov <m.banov7@gmail.com>
2021-11-16 17:23:23 +0100

    get plane coefficients

--- plane_detection/hello_world.py
+++ plane_detection/hello_world.py
@@ -7,29 +7,34 @@ import pcl.pcl_visualization
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", "-f", type=str, required=True)
-    
-    print("-------LOADING PCD-------")
     filename = parser.parse_args().file
+
+    print("-------LOADING PCD-------")
     cloud = pcl.load(filename)
 
     print("-------COMPUTING MODEL-------")
+    """
     model_p = pcl.SampleConsensusModelPlane(cloud)
     ransac = pcl.RandomSampleConsensus(model_p)
     ransac.set_DistanceThreshold(.017)
     ransac.computeModel()
-
-    print("-------GETTING INLIERS-------")
     inliers = ransac.get_Inliers()
+    """
+    seg = cloud.make_segmenter_normals(ksearch=10)
+    seg.set_optimize_coefficients(True)
+    seg.set_model_type(pcl.SACMODEL_NORMAL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    seg.set_distance_threshold(0.05)
+    seg.set_normal_distance_weight(0.005)
+    seg.set_max_iterations(100)
+    inliers, coefficients = seg.segment()
+    print(coefficients)
 
     if len(inliers) != 0:
-        print("-------GETTING FINAL POINTS-------")
-        finalpoints = np.zeros((len(inliers), 3), dtype=np.float32)
-        
-        for i in range(0, len(inliers)):
-            finalpoints[i][0] = cloud[inliers[i]][0]
-            finalpoints[i][1] = cloud[inliers[i]][1]
-            finalpoints[i][2] = cloud[inliers[i]][2]
-
+        cloud_points = np.full((cloud.size, 3), cloud, dtype=np.float32)
+        """
+        print("-------GETTING PLANE POINTS-------")
+        finalpoints = np.copy(cloud_points[inliers])
         final = pcl.PointCloud()
         final.from_array(finalpoints)
 
@@ -37,13 +42,17 @@ def main():
         chull = final.make_ConcaveHull()
         # chull.set_Alpha(0.5)
         chull.set_Alpha(0.07)
-        cloud_hull = chull.reconstruct()
+        final = chull.reconstruct()
+        """
+        print("-------REMOVING PLANE POINTS-------")
+        finalpoints = np.delete(cloud_points, inliers, axis=0)
+        final = pcl.PointCloud()
+        final.from_array(finalpoints)
 
-        # creates the visualization object and adds all of the inliers
         print("-------VISUALIZING-------")
         viewer = pcl.pcl_visualization.PCLVisualizering('3D Viewer')
         viewer.SetBackgroundColor(0, 0, 0)
-        viewer.AddPointCloud(cloud_hull, b'inliers cloud')
+        viewer.AddPointCloud(final, b'inliers cloud')
         viewer.SetPointCloudRenderingProperties(pcl.pcl_visualization.PCLVISUALIZER_POINT_SIZE, 3, b'inliers cloud')
         viewer.InitCameraParameters()
 
